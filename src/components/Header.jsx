a47ae850d3a9ac4d18e9ec38a9ce3874125031ee
Eddie <user@users-MacBook-Pro.local>
2023-07-08 02:44:57 -0400

    Corrected everything

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,14 @@
-<<<<<<< HEAD
 import { Box, HStack, useBreakpointValue } from "@chakra-ui/react";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-=======
-import React, { useState, useRef, useEffect } from "react";
-import { Box, HStack } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
->>>>>>> 2e3cf6a23b48e38e01211e35e882c4f51f841cff
 
 const socials = [
   {
     icon: faEnvelope,
-    url: "mailto: eidde_606@yahoo.com",
+    url: "mailto:eidde_606@yahoo.com",
   },
   {
     icon: faLinkedin,
@@ -57,11 +48,8 @@ const Navbar = () => {
     };
   }, [prevScrollPos, visible]);
 
-<<<<<<< HEAD
   const isMobile = useBreakpointValue({ base: true, md: false });
 
-=======
->>>>>>> 2e3cf6a23b48e38e01211e35e882c4f51f841cff
   return (
     <Box
       position="fixed"
@@ -72,30 +60,17 @@ const Navbar = () => {
       ref={headerRef}
       zIndex="999"
       transition="transform 0.3s ease"
-<<<<<<< HEAD
       transform={visible ? "translateY(0)" : "translateY(-200px)"}
     >
       <Box color="white" maxWidth="2000px" margin="0 auto">
         <HStack
           px={isMobile ? 4 : 16}
           py={isMobile ? 2 : 4}
-=======
-      transform="translateY(0)"
-    >
-      <Box color="white" maxWidth="2000px" margin="0 auto">
-        <HStack
-          px={16}
-          py={4}
->>>>>>> 2e3cf6a23b48e38e01211e35e882c4f51f841cff
           justifyContent="space-between"
           alignItems="center"
         >
           <nav>
-<<<<<<< HEAD
             <HStack spacing={isMobile ? 4 : 8}>
-=======
-            <HStack spacing={8}>
->>>>>>> 2e3cf6a23b48e38e01211e35e882c4f51f841cff
               {socials.map((social, index) => (
                 <a
                   href={social.url}
@@ -103,14 +78,10 @@ const Navbar = () => {
                   rel="noopener noreferrer"
                   key={index}
                 >
-<<<<<<< HEAD
                   <FontAwesomeIcon
                     icon={social.icon}
                     size={isMobile ? "lg" : "2x"}
                   />
-=======
-                  <FontAwesomeIcon icon={social.icon} size="2x" />
->>>>>>> 2e3cf6a23b48e38e01211e35e882c4f51f841cff
                 </a>
               ))}
             </HStack>
